77df0edd1a5984b0a5a35f3888d44f0f583be1f4
onurbzkn <onurbozkan472@gmail.com>
2026-02-27 04:54:40 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('dice-store').then((cache) => cache.addAll([
+      '/Zar/',
+      '/Zar/index.html',
+      '/Zar/manifest.json',
+      '/Zar/icon-512.png'
+    ]))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
 
